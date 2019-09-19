738e80e2a773e598503ffef060c1e90590aab3e5
Nicholas Truson <nicholas.truson@gmail.com>
2019-09-19 13:53:26 -0700

    initial

--- client/src/Movies/MovieUpdate.js
+++ client/src/Movies/MovieUpdate.js
@@ -1,12 +1,34 @@
 import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const MovieUpdate = () => {
+// INITIAL
+
+const MovieUpdate = props => {
+
+    const initialItem = {
+
+        id: props.match.params.id,
+        title: "",
+        director: "",
+        metascore: "",
+        
+    };
+
+// STATES
+
+
+
+// EFFECTS
+
+
+
   return (
 
     <div>
       <h1>Setup</h1>
     </div>
-    
+
   );
 };
 
